ef85e5d373f3357b45021bd171440d2355ea0a43
Davide Serra <daviserra@gmail.com>
2025-12-28 12:05:00 +0100

    Comment out flask-migrate temporarily

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_socketio import SocketIO
-from flask_migrate import Migrate
+# from flask_migrate import Migrate
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 from sqlalchemy.orm import DeclarativeBase
@@ -47,7 +47,7 @@ app.config["SQLALCHEMY_ENGINE_OPTIONS"] = {
 
 # Initialize database and migrations
 db = SQLAlchemy(app, model_class=Base)
-migrate = Migrate(app, db)
+# migrate = Migrate(app, db)
 
 # Initialize rate limiter
 limiter = Limiter(
